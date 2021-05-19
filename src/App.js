7fd491ee2f4b1ca8d1a6c80dfd915fde4f3da322
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-05-19 16:47:25 -0700

    Finished setting up App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,98 @@ const initialFormErrors = {
 const initialUsers = [];
 const initialDisabled = true;
 
+export default function App () {
+  // useState //
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
 
+  // helpers //
+
+  const getUsers = () => {
+    axios
+      .get("https://reqres.in/api/users")
+      .then((response) => {
+        setUsers(response?.data?.data);
+      })
+      .catch((error) => {
+        console.log("THE ERROR: ", error)
+      });
+  };
+
+  const postNewUser = (newUser) => {
+    console.log("post", newUser);
+    axios
+      .post("https://reqres.in/api/users")
+      .then((response) => {
+        console.log("THE RESPONSE: ", response)
+        setUsers([response?.data, ...users]);
+      })
+      .catch((error) => {
+        console.log("THE ERROR: ", error);
+      });
+  };
+
+  const inputChange = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((error) => {
+        setFormErrors({
+          ...formErrors, 
+          [name]: value,
+        });
+      });
+  };
+
+  const formSubmit = () => {
+    const newUser = {
+      username: formValues.username.trim(),
+      password: formValues.password.trim(),
+      email: formValues.email.trim(),
+    };
+    postNewUser(newUser);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
+
+  console.log("users", users);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Create a New User</h1>
+      </header>
+
+      <Form 
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+
+      {users.map(user => (
+        <User key={user.id} details={user} />
+      ))}
+    </div>
+  );
+}
 
 
 
